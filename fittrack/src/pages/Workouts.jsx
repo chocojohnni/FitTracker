ce27e49fbc3fd5bce0ea6a6964e5e521f21a3774
Johnny Tan <tannjohnny03@gmail.com>
2026-09-24 02:46:34 -0400

    Added Workouts function to display exercises in the according day.

--- fittrack/src/pages/Workouts.jsx
+++ fittrack/src/pages/Workouts.jsx
@@ -1,8 +1,28 @@
+import ExerciseCard from "../components/ExerciseCard";
+
 function Workouts() {
     return (
-        <main>
-            <h2>Workouts</h2>
-            <p>Your workouts will appear here.</p>
+        <main classNam="workout-page">
+            <section className="workout-header">
+                <h2>Push Day</h2>
+                <p>Chest • Shoulders • Triceps</p>
+            </section>
+
+            <section className="exercises">
+                <ExerciseCard name="Bench Press" />
+
+                <ExerciseCard name="Incline Dumbbell Press" />
+
+                <ExerciseCard name="Shoulder Press" />
+            </section>
+
+            <button className="add-exercise-button">
+                + Add Exercise
+            </button>
+
+            <button className="finish-workout-button">
+                Finish Workout
+            </button>
         </main>
     );
 }
