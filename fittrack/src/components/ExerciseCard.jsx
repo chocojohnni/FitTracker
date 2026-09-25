21c7e03db1288cc4c676095a411acf133e2a9f04
Johnny Tan <tannjohnny03@gmail.com>
2026-09-25 12:22:29 -0400

    Implemeneted React State to allow users to add a set with the working functionality of the + Add Set button

--- fittrack/src/components/ExerciseCard.jsx
+++ fittrack/src/components/ExerciseCard.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function ExerciseCard({ name }) {
+    const [sets, setSets] = useState([
+        { weight: 185, reps: 8},
+        { weight: 185, reps: 8},
+        { weight: 185, reps: 7}
+    ]);
+
+    function addSet() {
+        setSets([
+            ...sets,
+            { weight: 0, reps: 0 }
+        ]);
+    }
+
     return (
         <div className="exercise-card">
             <h3>{name}</h3>
@@ -9,25 +24,15 @@ function ExerciseCard({ name }) {
                 <span>Reps</span>
             </div>
 
-            <div className="set-row">
-                <span>1</span>
-                <span>185 lb</span>
-                <span>8</span>
-            </div>
+            {sets.map((set, index) => (
+                <div className="set-row" key={index}>
+                    <span>{index + 1}</span>
+                    <span>{set.weight} lb</span>
+                    <span>{set.reps}</span>
+                </div>
+            ))}
 
-            <div className="set-row">
-                <span>2</span>
-                <span>185 lb</span>
-                <span>8</span>
-            </div>
-
-            <div className="set-row">
-                <span>3</span>
-                <span>185 lb</span>
-                <span>7</span>
-            </div>
-
-            <button>+ Add Set</button>
+            <button onClick={addSet}>+ Add Set</button>
         </div>
     );
 }
